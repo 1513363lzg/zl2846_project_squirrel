475caaab4bc36d292476d1a265a9869135b1bb80
casper <liu.zhigeng@outlook.com>
2019-11-26 16:29:49 +0000

    view

--- myproject/map/views.py
+++ myproject/map/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
+from .model import Squirrel
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'map/detail.html', {'question': question})
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
 # Create your views here.
