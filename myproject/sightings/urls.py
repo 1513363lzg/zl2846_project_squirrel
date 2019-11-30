128f3bad081aa9fc7ad7a650a5ab0a9ba11e8a89
zl2838 <zl2838@columbia.edu>
2019-11-30 00:50:13 +0000

    add urls.py views.py all.html

---
+++ myproject/sightings/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+  
+from . import views
+
+urlpatterns =[
+        path('', views.index),
+        path('add/', views.add_squirrel),
+        path('stats/', views.all_squirrels),
+        path('<int:squirrel_id>/', views.squirrel_details),
+        ]
+
--- myproject/sightings/views.py
+++ myproject/sightings/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Squirrel
+def index(request):
+    Squirrels = Squirrel.objects.all()
+    context = {
+            'Squirrels': Squirrels,
+    }
+    return render(request, 'sightings/all.html', context)
+
+def all_pets(request):
+    pets = Pet.objects.all()
+    context = {
+            'pets': pets,
+    }
+    return render(request, 'adopt/all.html', context)
+
+def pet_details(request, pet_id):
+    pet = Pet.objects.get(id=pet_id)
+    return HttpResponse(pet.name)
+
