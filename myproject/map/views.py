a10943ffb454fe4c72a640918383ec1569d06924
casper <liu.zhigeng@outlook.com>
2019-11-30 03:38:00 +0000

    sightings

--- myproject/map/views.py
+++ myproject/map/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
-from .model import Squirrel
 def detail(request, question_id):
     try:
         question = Question.objects.get(pk=question_id)
--- myproject/sightings/views.py
+++ myproject/sightings/views.py
@@ -6,17 +6,24 @@ def index(request):
     Squirrels = Squirrel.objects.all()
     context = {
             'Squirrels': Squirrels,
-    }
+            }
     return render(request, 'sightings/all.html', context)
 
-def all_pets(request):
-    pets = Pet.objects.all()
-    context = {
-            'pets': pets,
-    }
-    return render(request, 'adopt/all.html', context)
+def add_squirrel(request,Squirrel_id):
+    pet = Squirrel.objects.get(id=Squirrel_id)
+    return HttpResponse(pet.Age)
 
-def pet_details(request, pet_id):
-    pet = Pet.objects.get(id=pet_id)
-    return HttpResponse(pet.name)
+
+def all_squirrels(request,Squirrel_id):
+    pet = Squirrel.objects.get(id=Squirrel_id)
+    return HttpResponse(pet.Age)
+
+
+def squirrel_details(request,Squirrel_id):
+    pet = Squirrel.objects.get(id=pet_id)
+    return HttpResponse(pet.Age)
+
+
+
+
 
