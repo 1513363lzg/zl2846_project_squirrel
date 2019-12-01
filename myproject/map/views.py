492656155ad20faca72f76fab4985f67724a1189
casper <liu.zhigeng@outlook.com>
2019-12-01 14:54:17 +0000

    map

--- myproject/map/views.py
+++ myproject/map/views.py
@@ -2,12 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, 'map/detail.html', {'question': question})
+def detail(request):
+
+    return render(request, 'map/map.html')
 
 
 def index(request):
