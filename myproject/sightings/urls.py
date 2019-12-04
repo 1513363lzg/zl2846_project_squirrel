9343ec50eadb5951a45700b5145a368cbd2c6e1a
zl2838 <zl2838@columbia.edu>
2019-12-04 00:24:26 +0000

    stats view html url

--- myproject/sightings/urls.py
+++ myproject/sightings/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns =[
         path('', views.index),
         path('add/', views.add_squirrel),
-        path('stats/', views.all_squirrels),
+        path('stats/', views.squirrel_stats),
         path('<int:Squirrel_id>/', views.squirrel_details),
         ]
 
--- myproject/sightings/views.py
+++ myproject/sightings/views.py
@@ -24,8 +24,17 @@ def squirrel_details(request, Squirrel_id):
     return HttpResponse({'Age: %a' %pet.Age,' Date: %a' %pet.Date})
 
 def squirrel_stats(request):
-    sightings_stats1=Squirrel.object.all().count
-    sightings_stats2=Squirrel.object.filter().count
-    sightings_stats3=
-    sightings_stats4=Squirrel.object.filter(Age=Adult).count
-    sightings_stats5=
+    sightings_stats1=Squirrel.object.all().count()
+    sightings_stats2=Squirrel.object.filter(PRIMARYFURCOLOR_CHOICES='Black').count()
+    sightings_stats3=Squirrel.object.filter(PRIMARYFURCOLOR_CHOICES='Gray').count()
+    sightings_stats4=Squirrel.object.filter(Age='Adult').count()
+    sightings_stats5=Squirrel.object.filter(Age='Juvenile').count()
+    context={'sightings_stats1':sightings_stats1,
+            'sightings_stats2':sightings_stats2,
+            'sightings_stats3':sightings_stats3,
+            'sightings_stats3':sightings_stats3,
+            'sightings_stats4':sightings_stats4,
+            'sightings_stats5':sightings_stats5,
+            }
+    return render(request, 'sightings/stats.html', context)
+
