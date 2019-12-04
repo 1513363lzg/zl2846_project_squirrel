edb648e9963e36d312ad952f8f5689c732ea95df
zl2838 <zl2838@columbia.edu>
2019-12-04 01:39:45 +0000

    change

--- myproject/sightings/views.py
+++ myproject/sightings/views.py
@@ -15,20 +15,19 @@ def index(request):
 def add_squirrel(request):
     return HttpResponse('we need to create a new html for edit')
 
-#just an example
 def all_squirrels(request):
     return HttpResponse('list all squirrels information')
 
-def squirrel_details(request, Squirrel_id):
-    pet = Squirrel.objects.get(id=Squirrel_id)
+def squirrel_details(request,Unique_squirrel_ID):
+    pet = Squirrel.objects.get(id=Unique_squirrel_ID)
     return HttpResponse({'Age: %a' %pet.Age,' Date: %a' %pet.Date})
 
 def squirrel_stats(request):
-    sightings_stats1=Squirrel.object.all().count()
-    sightings_stats2=Squirrel.object.filter(PRIMARYFURCOLOR_CHOICES='Black').count()
-    sightings_stats3=Squirrel.object.filter(PRIMARYFURCOLOR_CHOICES='Gray').count()
-    sightings_stats4=Squirrel.object.filter(Age='Adult').count()
-    sightings_stats5=Squirrel.object.filter(Age='Juvenile').count()
+    sightings_stats1=Squirrel.objects.all().count()
+    sightings_stats2=Squirrel.objects.filter(PRIMARYFURCOLOR_CHOICES='Black').count()
+    sightings_stats3=Squirrel.objects.filter(PRIMARYFURCOLOR_CHOICES='Gray').count()
+    sightings_stats4=Squirrel.objects.filter(Age='Adult').count()
+    sightings_stats5=Squirrel.objects.filter(Age='Juvenile').count()
     context={'sightings_stats1':sightings_stats1,
             'sightings_stats2':sightings_stats2,
             'sightings_stats3':sightings_stats3,
