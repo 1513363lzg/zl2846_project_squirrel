f34b69986aa4c32581efa7b96431f8702fe88d7d
casper <liu.zhigeng@outlook.com>
2019-12-04 01:56:41 +0000

    try

--- myproject/sightings/views.py
+++ myproject/sightings/views.py
@@ -24,8 +24,8 @@ def squirrel_details(request,Unique_squirrel_ID):
 
 def squirrel_stats(request):
     sightings_stats1=Squirrel.objects.all().count()
-    sightings_stats2=Squirrel.objects.filter(PRIMARYFURCOLOR_CHOICES='Black').count()
-    sightings_stats3=Squirrel.objects.filter(PRIMARYFURCOLOR_CHOICES='Gray').count()
+    sightings_stats2=Squirrel.objects.filter(Primary_fur_color='Black').count()
+    sightings_stats3=Squirrel.objects.filter(Primary_fur_color='Gray').count()
     sightings_stats4=Squirrel.objects.filter(Age='Adult').count()
     sightings_stats5=Squirrel.objects.filter(Age='Juvenile').count()
     context={'sightings_stats1':sightings_stats1,
